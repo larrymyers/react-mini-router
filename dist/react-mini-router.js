dacef9f058538fccd54cb63798db8be81d7deec3
Larry Myers <larry@larrymyers.com>
2014-08-06 21:22:51 -0500

    add bower config, generate dist builds for browser env

---
+++ dist/react-mini-router.js
@@ -0,0 +1,618 @@
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.ReactMiniRouter=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = {
+    RouterMixin: require('./lib/RouterMixin'),
+    navigate: require('./lib/navigate')
+};
+},{"./lib/RouterMixin":2,"./lib/navigate":4}],2:[function(require,module,exports){
+var pathToRegexp = require('path-to-regexp'),
+    urllite = require('urllite'),
+    detect = require('./detect');
+
+module.exports = {
+
+    getInitialState: function() {
+        return {
+            path: this.props.path,
+            useHistory: this.props.history && detect.hasPushState
+        };
+    },
+
+    componentWillMount: function() {
+        this.setState({ _routes: processRoutes(this.routes || {}, this) });
+    },
+
+    componentDidMount: function() {
+        this.getDOMNode().addEventListener('click', this.handleClick, false);
+
+        if (this.state.useHistory) {
+            window.addEventListener('popstate', this.onPopState, false);
+        } else {
+            window.addEventListener('hashchange', this.onPopState, false);
+        }
+    },
+
+    componentWillUnmount: function() {
+        this.getDOMNode().removeEventListener('click', this.handleClick);
+
+        if (this.state.useHistory) {
+            window.removeEventListener('popstate', this.onPopState);
+        } else {
+            window.removeEventListener('hashchange', this.onPopState);
+        }
+    },
+
+    onPopState: function() {
+        var url = urllite(window.location.href),
+            hash = url.hash || '',
+            path = this.state.useHistory ? url.pathname : hash.slice(2);
+
+        if (path.length === 0) path = '/';
+
+        if (this.matchRoute(path)) {
+            this.setState({ path: path + url.search });
+        }
+    },
+
+    renderCurrentRoute: function() {
+        var path = this.state.path,
+            url;
+
+        if (path) {
+            url = urllite(path);
+        } else if (!path && detect.canUseDOM) {
+            url = urllite(window.location.href);
+            if (!this.state.useHistory) url = urllite(url.hash ? url.hash.slice(2) : '');
+        } else {
+            // TODO throw error? default to root?
+        }
+
+        var parsedPath = url.pathname;
+
+        if (!parsedPath || parsedPath.length === 0) parsedPath = '/';
+
+        var matchedRoute = this.matchRoute(parsedPath);
+
+        // TODO if matchedRoute is null, render a 404
+
+        return matchedRoute.handler.apply(this, matchedRoute.params.concat(url.query));
+    },
+
+    handleClick: function(evt) {
+        var self = this,
+            url = getHref(evt);
+
+        if (url && self.matchRoute(url.pathname)) {
+            evt.preventDefault();
+
+            // See: http://facebook.github.io/react/docs/interactivity-and-dynamic-uis.html
+            // Give any component event listeners a chance to fire in the current event loop,
+            // since they happen at the end of the bubbling phase. (Allows an onClick prop to
+            // work correctly on the event target <a/> component.)
+            setTimeout(function() {
+                var pathWithSearch = url.pathname + (url.search || '');
+                if (pathWithSearch.length === 0) pathWithSearch = '/';
+
+                if (self.state.useHistory) {
+                    window.history.pushState({}, '', pathWithSearch);
+                } else {
+                    window.location.hash = '!' + pathWithSearch;
+                }
+
+                self.setState({ path: pathWithSearch});
+            }, 0);
+        }
+    },
+
+    matchRoute: function(path) {
+        if (!path) return false;
+
+        var matchedRoute = {};
+
+        this.state._routes.some(function(route) {
+            var matches = route.pattern.exec(path);
+
+            if (matches) {
+                matchedRoute.handler = route.handler;
+                matchedRoute.params = matches.slice(1, route.params.length + 1);
+
+                return true;
+            }
+
+            return false;
+        });
+
+        return matchedRoute;
+    }
+
+};
+
+function getHref(evt) {
+    if (evt.defaultPrevented) {
+        return;
+    }
+
+    if (evt.metaKey || evt.ctrlKey || evt.shiftKey) {
+        return;
+    }
+
+    if (evt.button !== 0) {
+        return;
+    }
+
+    var elt = evt.target;
+
+    // Since a click could originate from a child element of the <a> tag,
+    // walk back up the tree to find it.
+    while (elt && elt.nodeName !== 'A') {
+        elt = elt.parentNode;
+    }
+
+    if (!elt) {
+        return;
+    }
+
+    if (elt.target && elt.target !== '_self') {
+        return;
+    }
+
+    if (!!elt.attributes.download) {
+        return;
+    }
+
+    var linkURL = urllite(elt.href);
+    var windowURL = urllite(window.location.href);
+
+    if (linkURL.protocol !== windowURL.protocol || linkURL.host !== windowURL.host) {
+        return;
+    }
+
+    return linkURL;
+}
+
+function processRoutes(routes, component) {
+    var patterns = [],
+        path, pattern, keys, handler, handlerFn;
+
+    for (path in routes) {
+        if (routes.hasOwnProperty(path)) {
+            keys = [];
+            pattern = pathToRegexp(path, keys);
+            handler = routes[path];
+            handlerFn = component[handler];
+
+            patterns.push({ pattern: pattern, params: keys, handler: handlerFn });
+        }
+    }
+
+    return patterns;
+}
+
+},{"./detect":3,"path-to-regexp":5,"urllite":6}],3:[function(require,module,exports){
+var canUseDOM = !!(
+    typeof window !== 'undefined' &&
+    window.document &&
+    window.document.createElement
+);
+
+module.exports = {
+    canUseDOM: canUseDOM,
+    hasPushState: canUseDOM && window.history && 'pushState' in window.history,
+    hasHashbang: function() {
+        return canUseDOM && window.location.hash.indexOf('#!') === 0;
+    }
+};
+
+},{}],4:[function(require,module,exports){
+var detect = require('./detect');
+
+module.exports = function triggerUrl(url, silent) {
+    if (detect.hasHashbang()) {
+        window.location.hash = '#!' + url;
+    } else if (detect.hasPushState) {
+        window.history.pushState({}, '', url);
+        if (!silent) window.dispatchEvent(new window.Event('popstate'));
+    } else {
+        console.error("Browser does not support pushState, and hash is missing a hashbang prefix!");
+    }
+};
+
+},{"./detect":3}],5:[function(require,module,exports){
+/**
+ * Expose `pathtoRegexp`.
+ */
+module.exports = pathtoRegexp;
+
+var PATH_REGEXP = new RegExp([
+  // Match already escaped characters that would otherwise incorrectly appear
+  // in future matches. This allows the user to escape special characters that
+  // shouldn't be transformed.
+  '(\\\\.)',
+  // Match Express-style parameters and un-named parameters with a prefix
+  // and optional suffixes. Matches appear as:
+  //
+  // "/:test(\\d+)?" => ["/", "test", "\d+", undefined, "?"]
+  // "/route(\\d+)" => [undefined, undefined, undefined, "\d+", undefined]
+  '([\\/.])?(?:\\:(\\w+)(?:\\(((?:\\\\.|[^)])*)\\))?|\\(((?:\\\\.|[^)])*)\\))([+*?])?',
+  // Match regexp special characters that should always be escaped.
+  '([.+*?=^!:${}()[\\]|\\/])'
+].join('|'), 'g');
+
+/**
+ * Escape the capturing group by escaping special characters and meaning.
+ *
+ * @param  {String} group
+ * @return {String}
+ */
+function escapeGroup (group) {
+  return group.replace(/([=!:$\/()])/g, '\\$1');
+}
+
+/**
+ * Normalize the given path string, returning a regular expression.
+ *
+ * An empty array should be passed in, which will contain the placeholder key
+ * names. For example `/user/:id` will then contain `["id"]`.
+ *
+ * @param  {(String|RegExp|Array)} path
+ * @param  {Array}                 keys
+ * @param  {Object}                options
+ * @return {RegExp}
+ */
+function pathtoRegexp (path, keys, options) {
+  keys = keys || [];
+  options = options || {};
+
+  var strict = options.strict;
+  var end = options.end !== false;
+  var flags = options.sensitive ? '' : 'i';
+  var index = 0;
+
+  if (path instanceof RegExp) {
+    // Match all capturing groups of a regexp.
+    var groups = path.source.match(/\((?!\?)/g) || [];
+
+    // Map all the matches to their numeric keys and push into the keys.
+    keys.push.apply(keys, groups.map(function (match, index) {
+      return {
+        name:      index,
+        delimiter: null,
+        optional:  false,
+        repeat:    false
+      };
+    }));
+
+    // Return the source back to the user.
+    return path;
+  }
+
+  if (Array.isArray(path)) {
+    // Map array parts into regexps and return their source. We also pass
+    // the same keys and options instance into every generation to get
+    // consistent matching groups before we join the sources together.
+    path = path.map(function (value) {
+      return pathtoRegexp(value, keys, options).source;
+    });
+
+    // Generate a new regexp instance by joining all the parts together.
+    return new RegExp('(?:' + path.join('|') + ')', flags);
+  }
+
+  // Alter the path string into a usable regexp.
+  path = path.replace(PATH_REGEXP, function (match, escaped, prefix, key, capture, group, suffix, escape) {
+    // Avoiding re-escaping escaped characters.
+    if (escaped) {
+      return escaped;
+    }
+
+    // Escape regexp special characters.
+    if (escape) {
+      return '\\' + escape;
+    }
+
+    var repeat   = suffix === '+' || suffix === '*';
+    var optional = suffix === '?' || suffix === '*';
+
+    keys.push({
+      name:      key || index++,
+      delimiter: prefix || '/',
+      optional:  optional,
+      repeat:    repeat
+    });
+
+    // Escape the prefix character.
+    prefix = prefix ? '\\' + prefix : '';
+
+    // Match using the custom capturing group, or fallback to capturing
+    // everything up to the next slash (or next period if the param was
+    // prefixed with a period).
+    capture = escapeGroup(capture || group || '[^' + (prefix || '\\/') + ']+?');
+
+    // Allow parameters to be repeated more than once.
+    if (repeat) {
+      capture = capture + '(?:' + prefix + capture + ')*';
+    }
+
+    // Allow a parameter to be optional.
+    if (optional) {
+      return '(?:' + prefix + '(' + capture + '))?';
+    }
+
+    // Basic parameter support.
+    return prefix + '(' + capture + ')';
+  });
+
+  // Check whether the path ends in a slash as it alters some match behaviour.
+  var endsWithSlash = path[path.length - 1] === '/';
+
+  // In non-strict mode we allow an optional trailing slash in the match. If
+  // the path to match already ended with a slash, we need to remove it for
+  // consistency. The slash is only valid at the very end of a path match, not
+  // anywhere in the middle. This is important for non-ending mode, otherwise
+  // "/test/" will match "/test//route".
+  if (!strict) {
+    path = (endsWithSlash ? path.slice(0, -2) : path) + '(?:\\/(?=$))?';
+  }
+
+  // In non-ending mode, we need prompt the capturing groups to match as much
+  // as possible by using a positive lookahead for the end or next path segment.
+  if (!end) {
+    path += strict && endsWithSlash ? '' : '(?=\\/|$)';
+  }
+
+  return new RegExp('^' + path + (end ? '$' : ''), flags);
+};
+
+},{}],6:[function(require,module,exports){
+(function() {
+  var urllite;
+
+  urllite = require('./core');
+
+  require('./extensions/resolve');
+
+  require('./extensions/relativize');
+
+  require('./extensions/normalize');
+
+  require('./extensions/toString');
+
+  module.exports = urllite;
+
+}).call(this);
+
+},{"./core":7,"./extensions/normalize":8,"./extensions/relativize":9,"./extensions/resolve":10,"./extensions/toString":11}],7:[function(require,module,exports){
+(function() {
+  var URL, URL_PATTERN, defaults, urllite,
+    __hasProp = {}.hasOwnProperty,
+    __slice = [].slice;
+
+  URL_PATTERN = /^(?:(?:([^:\/?\#]+:)\/+|(\/\/))(?:([a-z0-9-\._~%]+)(?::([a-z0-9-\._~%]+))?@)?(([a-z0-9-\._~%!$&'()*+,;=]+)(?::([0-9]+))?)?)?([^?\#]*?)(\?[^\#]*)?(\#.*)?$/;
+
+  urllite = function(raw, opts) {
+    return urllite.URL.parse(raw, opts);
+  };
+
+  urllite.URL = URL = (function() {
+    function URL(props) {
+      var k, v;
+      for (k in props) {
+        if (!__hasProp.call(props, k)) continue;
+        v = props[k];
+        this[k] = v;
+      }
+    }
+
+    URL.parse = function(raw) {
+      var m, pathname, protocol;
+      m = raw.toString().match(URL_PATTERN);
+      pathname = m[8] || '';
+      protocol = m[1];
+      return urllite._createURL({
+        protocol: protocol,
+        username: m[3],
+        password: m[4],
+        hostname: m[6],
+        port: m[7],
+        pathname: protocol && pathname.charAt(0) !== '/' ? "/" + pathname : pathname,
+        search: m[9],
+        hash: m[10],
+        isSchemeRelative: m[2] != null
+      });
+    };
+
+    return URL;
+
+  })();
+
+  defaults = {
+    protocol: '',
+    username: '',
+    password: '',
+    host: '',
+    hostname: '',
+    port: '',
+    pathname: '',
+    search: '',
+    hash: '',
+    origin: '',
+    isSchemeRelative: false
+  };
+
+  urllite._createURL = function() {
+    var base, bases, k, props, v, _i, _len, _ref, _ref1;
+    bases = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+    props = {};
+    for (_i = 0, _len = bases.length; _i < _len; _i++) {
+      base = bases[_i];
+      for (k in defaults) {
+        if (!__hasProp.call(defaults, k)) continue;
+        v = defaults[k];
+        props[k] = (_ref = (_ref1 = base[k]) != null ? _ref1 : props[k]) != null ? _ref : v;
+      }
+    }
+    props.host = props.hostname && props.port ? "" + props.hostname + ":" + props.port : props.hostname ? props.hostname : '';
+    props.origin = props.protocol ? "" + props.protocol + "//" + props.host : '';
+    props.isAbsolutePathRelative = !props.host && props.pathname.charAt(0) === '/';
+    props.isPathRelative = !props.host && props.pathname.charAt(0) !== '/';
+    props.isRelative = props.isSchemeRelative || props.isAbsolutePathRelative || props.isPathRelative;
+    props.isAbsolute = !props.isRelative;
+    return new urllite.URL(props);
+  };
+
+  module.exports = urllite;
+
+}).call(this);
+
+},{}],8:[function(require,module,exports){
+(function() {
+  var URL, urllite;
+
+  urllite = require('../core');
+
+  URL = urllite.URL;
+
+  URL.prototype.normalize = function() {
+    var m, pathname;
+    pathname = this.pathname;
+    while (m = /^(.*?)[^\/]+\/\.\.\/*(.*)$/.exec(pathname)) {
+      pathname = "" + m[1] + m[2];
+    }
+    if (this.host && pathname.indexOf('..') !== -1) {
+      throw new Error('Path is behind root.');
+    }
+    return urllite._createURL(this, {
+      pathname: pathname
+    });
+  };
+
+}).call(this);
+
+},{"../core":7}],9:[function(require,module,exports){
+(function() {
+  var URL, urllite;
+
+  urllite = require('../core');
+
+  require('./resolve');
+
+  URL = urllite.URL;
+
+  URL.prototype.relativize = function(other) {
+    var c, i, newSegments, otherSegments, url, urlSegments, _i, _len, _ref;
+    if (this.isPathRelative) {
+      return new urllite.URL(this);
+    }
+    if (typeof other === 'string') {
+      other = urllite(other);
+    }
+    url = this.resolve(other);
+    if (url.origin && url.origin !== other.origin) {
+      throw new Error("Origins don't match (" + url.origin + " and " + other.origin + ")");
+    } else if (!other.isAbsolute && !other.isAbsolutePathRelative) {
+      throw new Error("Other URL (<" + other + ">) is neither absolute nor absolute path relative.");
+    }
+    otherSegments = other.pathname.split('/').slice(1);
+    urlSegments = url.pathname.split('/').slice(1);
+    for (i = _i = 0, _len = urlSegments.length; _i < _len; i = ++_i) {
+      c = urlSegments[i];
+      if (!(c === otherSegments[i] && (urlSegments.length > (_ref = i + 1) && _ref < otherSegments.length))) {
+        break;
+      }
+    }
+    newSegments = urlSegments.slice(i);
+    while (i < otherSegments.length - 1) {
+      if (otherSegments[i]) {
+        newSegments.unshift('..');
+      }
+      i++;
+    }
+    if (newSegments.length === 1) {
+      newSegments = newSegments[0] === otherSegments[i] ? [''] : newSegments[0] === '' ? ['.'] : newSegments;
+    }
+    return urllite._createURL({
+      pathname: newSegments.join('/'),
+      search: url.search,
+      hash: url.hash
+    });
+  };
+
+}).call(this);
+
+},{"../core":7,"./resolve":10}],10:[function(require,module,exports){
+(function() {
+  var URL, copyProps, oldParse, urllite,
+    __slice = [].slice;
+
+  urllite = require('../core');
+
+  require('./normalize');
+
+  URL = urllite.URL;
+
+  oldParse = URL.parse;
+
+  copyProps = function() {
+    var prop, props, source, target, _i, _len;
+    target = arguments[0], source = arguments[1], props = 3 <= arguments.length ? __slice.call(arguments, 2) : [];
+    for (_i = 0, _len = props.length; _i < _len; _i++) {
+      prop = props[_i];
+      target[prop] = source[prop];
+    }
+    return target;
+  };
+
+  URL.parse = function(raw, opts) {
+    var base, url;
+    if (base = opts != null ? opts.base : void 0) {
+      delete opts.base;
+    }
+    url = oldParse(raw, opts);
+    if (base) {
+      return url.resolve(base);
+    } else {
+      return url;
+    }
+  };
+
+  URL.prototype.resolve = function(base) {
+    var p, prefix;
+    if (this.isAbsolute) {
+      return new urllite.URL(this);
+    }
+    if (typeof base === 'string') {
+      base = urllite(base);
+    }
+    p = {};
+    if (this.isSchemeRelative) {
+      copyProps(p, this, 'username', 'password', 'host', 'hostname', 'port', 'pathname', 'search', 'hash');
+      p.isSchemeRelative = !(p.protocol = base.protocol);
+    } else if (this.isAbsolutePathRelative || this.isPathRelative) {
+      copyProps(p, this, 'search', 'hash');
+      copyProps(p, base, 'protocol', 'username', 'password', 'host', 'hostname', 'port');
+      p.pathname = this.isPathRelative ? base.pathname.slice(0, -1) === '/' ? "" + base.pathname + "/" + this.pathname : (prefix = base.pathname.split('/').slice(0, -1).join('/'), prefix ? "" + prefix + "/" + this.pathname : this.pathname) : this.pathname;
+    }
+    return urllite._createURL(p).normalize();
+  };
+
+}).call(this);
+
+},{"../core":7,"./normalize":8}],11:[function(require,module,exports){
+(function() {
+  var URL, urllite;
+
+  urllite = require('../core');
+
+  URL = urllite.URL;
+
+  URL.prototype.toString = function() {
+    var authority, prefix, userinfo;
+    prefix = this.isSchemeRelative ? '//' : this.protocol === 'file:' ? "" + this.protocol + "///" : this.protocol ? "" + this.protocol + "//" : '';
+    userinfo = this.password ? "" + this.username + ":" + this.password : this.username ? "" + this.username : '';
+    authority = userinfo ? "" + userinfo + "@" + this.host : this.host ? "" + this.host : '';
+    return "" + prefix + authority + this.pathname + this.search + this.hash;
+  };
+
+}).call(this);
+
+},{"../core":7}]},{},[1])(1)
+});
