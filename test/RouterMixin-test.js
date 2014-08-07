df2c13bc08cee09c7980ed0e903d91419aa1860d
Larry Myers <larry@larrymyers.com>
2014-08-07 15:09:23 -0500

    So phantomjs doesn't have Function.prototype.bind, which is sad. Use Chrome as default browser for karma runs.

--- test/RouterMixin-test.js
+++ test/RouterMixin-test.js
@@ -1,7 +1,33 @@
-var assert = require('assert');
+var assert = require('assert'),
+    React = require('react'),
+    RouterMixin = require('./../lib/RouterMixin');
+
+var App = React.createClass({
+
+    mixins: [RouterMixin],
+
+    routes: {
+        '/': 'home'
+    },
+
+    render: function() {
+        return this.renderCurrentRoute();
+    },
+
+    home: function() {
+        return React.DOM.div(null, 'test');
+    }
+
+});
 
 describe('RouterMixin', function() {
 
-    it('Should parse the routes on the component when mounting.');
+    it('Should parse the routes on the component when mounting.', function() {
+        var app = App({ path: '/' });
+        var html = React.renderComponentToString(app);
+
+        assert.equal(html, '<div data-reactid=".0" data-react-checksum="-1097856244">test</div>');
+        assert.equal(app._routes.length, 1);
+    });
 
 });
