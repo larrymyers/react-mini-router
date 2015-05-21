7d354e01b3e6687d136d7f18077a83372358820a
Larry Myers <larry@larrymyers.com>
2015-05-20 19:38:54 -0500

    create release for 1.1.5 to reflect new build using webpack

--- dist/react-mini-router.js
+++ dist/react-mini-router.js
@@ -1,4 +1,4 @@
-/*! ReactMiniRouter 1.1.4 - https://github.com/larrymyers/react-mini-router */
+/*! ReactMiniRouter 1.1.5 - https://github.com/larrymyers/react-mini-router */
 var ReactMiniRouter =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -56,9 +56,9 @@ var ReactMiniRouter =
 /***/ function(module, exports, __webpack_require__) {
 
 	var React = __webpack_require__(3),
-	    EventListener = __webpack_require__(6),
+	    EventListener = __webpack_require__(7),
 	    pathToRegexp = __webpack_require__(5),
-	    urllite = __webpack_require__(7),
+	    urllite = __webpack_require__(6),
 	    detect = __webpack_require__(4);
 
 	var PropValidation = {
@@ -556,6 +556,77 @@ var ReactMiniRouter =
 /* 6 */
 /***/ function(module, exports, __webpack_require__) {
 
+	(function() {
+	  var URL, URL_PATTERN, defaults, urllite,
+	    __hasProp = {}.hasOwnProperty;
+
+	  URL_PATTERN = /^(?:(?:([^:\/?\#]+:)\/+|(\/\/))(?:([a-z0-9-\._~%]+)(?::([a-z0-9-\._~%]+))?@)?(([a-z0-9-\._~%!$&'()*+,;=]+)(?::([0-9]+))?)?)?([^?\#]*?)(\?[^\#]*)?(\#.*)?$/;
+
+	  urllite = function(raw, opts) {
+	    return urllite.URL.parse(raw, opts);
+	  };
+
+	  urllite.URL = URL = (function() {
+	    function URL(props) {
+	      var k, v, _ref;
+	      for (k in defaults) {
+	        if (!__hasProp.call(defaults, k)) continue;
+	        v = defaults[k];
+	        this[k] = (_ref = props[k]) != null ? _ref : v;
+	      }
+	      this.host || (this.host = this.hostname && this.port ? "" + this.hostname + ":" + this.port : this.hostname ? this.hostname : '');
+	      this.origin || (this.origin = this.protocol ? "" + this.protocol + "//" + this.host : '');
+	      this.isAbsolutePathRelative = !this.host && this.pathname.charAt(0) === '/';
+	      this.isPathRelative = !this.host && this.pathname.charAt(0) !== '/';
+	      this.isRelative = this.isSchemeRelative || this.isAbsolutePathRelative || this.isPathRelative;
+	      this.isAbsolute = !this.isRelative;
+	    }
+
+	    URL.parse = function(raw) {
+	      var m, pathname, protocol;
+	      m = raw.toString().match(URL_PATTERN);
+	      pathname = m[8] || '';
+	      protocol = m[1];
+	      return new urllite.URL({
+	        protocol: protocol,
+	        username: m[3],
+	        password: m[4],
+	        hostname: m[6],
+	        port: m[7],
+	        pathname: protocol && pathname.charAt(0) !== '/' ? "/" + pathname : pathname,
+	        search: m[9],
+	        hash: m[10],
+	        isSchemeRelative: m[2] != null
+	      });
+	    };
+
+	    return URL;
+
+	  })();
+
+	  defaults = {
+	    protocol: '',
+	    username: '',
+	    password: '',
+	    host: '',
+	    hostname: '',
+	    port: '',
+	    pathname: '',
+	    search: '',
+	    hash: '',
+	    origin: '',
+	    isSchemeRelative: false
+	  };
+
+	  module.exports = urllite;
+
+	}).call(this);
+
+
+/***/ },
+/* 7 */
+/***/ function(module, exports, __webpack_require__) {
+
 	/* WEBPACK VAR INJECTION */(function(process) {/**
 	 * Copyright 2013-2014 Facebook, Inc.
 	 *
@@ -644,77 +715,6 @@ var ReactMiniRouter =
 	module.exports = EventListener;
 
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(10)))
-
-/***/ },
-/* 7 */
-/***/ function(module, exports, __webpack_require__) {
-
-	(function() {
-	  var URL, URL_PATTERN, defaults, urllite,
-	    __hasProp = {}.hasOwnProperty;
-
-	  URL_PATTERN = /^(?:(?:([^:\/?\#]+:)\/+|(\/\/))(?:([a-z0-9-\._~%]+)(?::([a-z0-9-\._~%]+))?@)?(([a-z0-9-\._~%!$&'()*+,;=]+)(?::([0-9]+))?)?)?([^?\#]*?)(\?[^\#]*)?(\#.*)?$/;
-
-	  urllite = function(raw, opts) {
-	    return urllite.URL.parse(raw, opts);
-	  };
-
-	  urllite.URL = URL = (function() {
-	    function URL(props) {
-	      var k, v, _ref;
-	      for (k in defaults) {
-	        if (!__hasProp.call(defaults, k)) continue;
-	        v = defaults[k];
-	        this[k] = (_ref = props[k]) != null ? _ref : v;
-	      }
-	      this.host || (this.host = this.hostname && this.port ? "" + this.hostname + ":" + this.port : this.hostname ? this.hostname : '');
-	      this.origin || (this.origin = this.protocol ? "" + this.protocol + "//" + this.host : '');
-	      this.isAbsolutePathRelative = !this.host && this.pathname.charAt(0) === '/';
-	      this.isPathRelative = !this.host && this.pathname.charAt(0) !== '/';
-	      this.isRelative = this.isSchemeRelative || this.isAbsolutePathRelative || this.isPathRelative;
-	      this.isAbsolute = !this.isRelative;
-	    }
-
-	    URL.parse = function(raw) {
-	      var m, pathname, protocol;
-	      m = raw.toString().match(URL_PATTERN);
-	      pathname = m[8] || '';
-	      protocol = m[1];
-	      return new urllite.URL({
-	        protocol: protocol,
-	        username: m[3],
-	        password: m[4],
-	        hostname: m[6],
-	        port: m[7],
-	        pathname: protocol && pathname.charAt(0) !== '/' ? "/" + pathname : pathname,
-	        search: m[9],
-	        hash: m[10],
-	        isSchemeRelative: m[2] != null
-	      });
-	    };
-
-	    return URL;
-
-	  })();
-
-	  defaults = {
-	    protocol: '',
-	    username: '',
-	    password: '',
-	    host: '',
-	    hostname: '',
-	    port: '',
-	    pathname: '',
-	    search: '',
-	    hash: '',
-	    origin: '',
-	    isSchemeRelative: false
-	  };
-
-	  module.exports = urllite;
-
-	}).call(this);
-
 
 /***/ },
 /* 8 */
